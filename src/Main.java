ab2c9facf27df67d10b6d0aab093887cc0f500dc
BOLUWAJI <create1666@gmail.com>
2023-01-29 23:17:54 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Date now = new Date();
+        long time = now.getTime();
+
+        System.out.println(time);
+
+
+    }
+}
